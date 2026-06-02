397ceb4bcaab954f4913bfda993502e83783287a
ibailopeez <ibailopezbastida@gmail.com>
2026-06-02 14:29:26 +0200

    eliminación de archivo innecesario

--- static/js/checkout.js
+++
@@ -1,9 +0,0 @@
-function rellenarDireccion(calle, portal, piso, puerta, localidad, provincia, cp) {
-    document.getElementById('calle_entrega').value = calle;
-    document.getElementById('portal_entrega').value = portal;
-    document.getElementById('piso_entrega').value = piso;
-    document.getElementById('puerta_entrega').value = puerta;
-    document.getElementById('localidad_entrega').value = localidad;
-    document.getElementById('provincia_entrega').value = provincia;
-    document.getElementById('cp_entrega').value = cp;
-}
